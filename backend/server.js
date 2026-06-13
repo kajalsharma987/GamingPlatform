6c41a31cb88c3a2846b6e7dd8e8728161b18bb38
kajalsharma987 <7409kajal@gmail.com>
2026-06-13 16:46:55 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -63,6 +63,7 @@ app.use((err, req, res, next) => {
 
 initSockets(server);
 
-server.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
